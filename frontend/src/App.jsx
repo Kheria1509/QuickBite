c92b2f30f0155d2ffc6365ef2c7e1b49e88690e1
kheria1509 <amankheria09@gmail.com>
2024-10-15 23:09:16 +0530

    setup github

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Home from './pages/Home/Home'
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Cart' element={<Cart/>} />
+        <Route path='/Order' element={<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
