702c6d0ccc012bb68b66512d93f2a38fa32e1fb8
kheria1509 <amankheria09@gmail.com>
2024-10-29 00:05:20 +0530

    Done with the login/singup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Home from './pages/Home/Home'
+import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/Loginpopup'
 const App = () => {
+
+const [showLogin,setShowLogin]= useState(false)
+
   return (
-    <div className='app'>
-      <Navbar/>
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
+     <div className='app'>
+      <Navbar setShowLogin={setShowLogin} />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Cart' element={<Cart/>} />
         <Route path='/Order' element={<PlaceOrder/>} />
       </Routes>
     </div>
+    <Footer/>
+    
+    </>
+   
   )
 }
 
