2c85d211cb8c75a10b8b199ba7af50bdf8bc9b4a
kheria1509 <amankheria09@gmail.com>
2025-04-19 01:24:48 +0530

    import chanhed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
-import LoginPopup from './components/LoginPopup/Loginpopup'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 import Verify from "./pages/Verify/Verify";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
